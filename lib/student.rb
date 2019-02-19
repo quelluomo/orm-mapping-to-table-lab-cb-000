21e0296c6959f8c3e7396bbd77d5cdb25c18d290
Matteo Muscas <thatman.j@gmail.com>
2019-02-19 15:27:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,4 +24,10 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
 end
